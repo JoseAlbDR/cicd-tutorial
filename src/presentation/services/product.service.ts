b4d8925ad8de77313cf558426699501ad19ac9d6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-15 12:23:09 +0100

    feat: productService

---
+++ src/presentation/services/product.service.ts
@@ -0,0 +1,55 @@
+import { ProductModel } from '../../data';
+import { CustomError, PaginationDto } from '../../domain';
+import { ProductEntity } from '../../domain';
+
+export class ProductService {
+  constructor() {}
+
+  public async getProducts(paginationDto: PaginationDto) {
+    const { limit, page } = paginationDto;
+
+    try {
+      const [total, products] = await Promise.all([
+        ProductModel.countDocuments(),
+        ProductModel.find()
+          .skip((page - 1) * limit)
+          .limit(limit)
+          .populate('createdBy', 'name email'),
+      ]);
+
+      const maxPages = Math.ceil(total / limit);
+
+      const productEntities = products.map((product) => ({
+        ...ProductEntity.fromObject({
+          id: product.id,
+          name: product.name,
+          onSale: product.onSale,
+          price: product.price,
+          tags: product.tags,
+          createdBy: product.createdBy,
+          image: product.image,
+        }),
+      }));
+
+      return {
+        currentPage: page,
+        maxPages,
+        limit,
+        total,
+        next:
+          page + 1 <= maxPages
+            ? `/api/v1/products?page=${page + 1}&limit=${limit}`
+            : null,
+        prev:
+          page - 1 > 0
+            ? `/api/v1/products?page=${page - 1}&limit=${limit}`
+            : null,
+        products: productEntities,
+      };
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.log(error);
+      throw CustomError.internalServer();
+    }
+  }
+}
