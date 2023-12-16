fd8b1bab695025811be947ea1a3644becf8945e1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-16 18:06:06 +0100

    feat: SignupDto

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -4,3 +4,4 @@ export * from './dtos/shared/pagination.dto';
 export * from './errors/error.handler';
 export * from './dtos/products/create-product.dto';
 export * from './dtos/shared/filters.dto';
+export * from './dtos/auth/signup.dto';
