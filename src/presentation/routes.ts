3da141742a52adbf7e9a33b2303264f3d80dfe76
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-16 18:07:41 +0100

    feat: add authRoutes to appRoutes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ProductRoutes } from './api/products/routes';
 import { TagsRoutes } from './api/tags/routes';
 import { ProductWebRoutes } from './web/products/routes';
+import { AuthRoutes } from './api/auth/routes';
 
 export class AppRoutes {
   static get routes(): Router {
@@ -10,6 +11,7 @@ export class AppRoutes {
     // API routes
     router.use('/api/v1/products', ProductRoutes.routes);
     router.use('/api/v1/tags', TagsRoutes.routes);
+    router.use('/api/v1/auth', AuthRoutes.routes);
 
     // ViEWS routes
     router.use('/', ProductWebRoutes.routes);
