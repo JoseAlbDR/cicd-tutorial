3291953b8384dafb6f051a8eb0469de9f02fa0a2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-16 18:06:40 +0100

    feat: auth service signup

---
+++ src/presentation/api/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { UserModel } from '../../../data';
+import { CustomError } from '../../../domain';
+import { SignupDto } from '../../../domain/dtos/auth/signup.dto';
+
+export class AuthService {
+  constructor() {}
+
+  public async signup(signupDto: SignupDto) {
+    try {
+      const userExist = await UserModel.findOne({ email: signupDto.email });
+
+      if (userExist)
+        throw CustomError.badRequest('Email already exists, try another one');
+
+      const user = new UserModel(signupDto);
+      await user.save();
+      return user;
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+      console.log(error);
+      throw CustomError.internalServer();
+    }
+  }
+}
