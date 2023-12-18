34895da7933c5ca57430e82932263ab9439dc69c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-18 17:02:08 +0100

    feat: add FileUploadRoutes to app routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,7 @@ import { ProductRoutes } from './api/products/routes';
 import { TagsRoutes } from './api/tags/routes';
 import { ProductWebRoutes } from './web/products/routes';
 import { AuthRoutes } from './api/auth/routes';
+import { FileUploadRoutes } from './api/fileupload/routes';
 
 export class AppRoutes {
   static get routes(): Router {
@@ -12,7 +13,7 @@ export class AppRoutes {
     router.use('/api/v1/products', ProductRoutes.routes);
     router.use('/api/v1/tags', TagsRoutes.routes);
     router.use('/api/v1/auth', AuthRoutes.routes);
-    router.use('/api/v1/images');
+    router.use('/api/v1/images', FileUploadRoutes.routes);
 
     // ViEWS routes
     router.use('/', ProductWebRoutes.routes);
