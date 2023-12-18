692ab00be7ca37b67c77d1e8fd2a53a13d82685c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-18 17:01:22 +0100

    feat: complete FileUploadRoutes

--- src/presentation/api/fileupload/routes.ts
+++ src/presentation/api/fileupload/routes.ts
@@ -1,10 +1,34 @@
 import { Router } from 'express';
+import { ErrorHandler } from '../../../domain';
+import { FileUploadService } from '../services/file-upload.service';
+import { FileUploadController } from './controller';
+import { FileUploadMiddleware } from '../../middlewares/file-upload.middleware';
 
 export class FileUploadRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get('/single/:destiny');
+    const errorHandler = new ErrorHandler();
+    const fileUploadService = new FileUploadService();
+    const fileUploadController = new FileUploadController(
+      fileUploadService,
+      errorHandler
+    );
+
+    router.use([
+      FileUploadMiddleware.containFiles,
+      FileUploadMiddleware.validateType(['users', 'products']),
+      FileUploadMiddleware.validateExtension([
+        'png',
+        'jpg',
+        'jpeg',
+        'gif',
+        'webp',
+      ]),
+    ]);
+
+    router.get('/single/:destiny', fileUploadController.uploadSingleFile);
+    router.get('/multiple/:destiny', fileUploadController.uploadMultipleFiles);
 
     return router;
   }
