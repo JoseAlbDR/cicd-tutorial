7286809b964791c1a1c78b10f879e7619daf6356
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-29 20:10:13 +0100

    feat: add maxConcurrency: 1 option in jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  maxConcurrency: 1,
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
