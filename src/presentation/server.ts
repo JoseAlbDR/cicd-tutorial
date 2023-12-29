2b1e99c1d344487f191692ac2bf20801e39558d7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-29 20:11:19 +0100

    feat: handle server listen error and now server close return a promise

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -70,14 +70,26 @@ export class Server {
     //   );
     //   res.sendFile(indexPath);
     // });
-
     this.serverListener = this.app.listen(this.port, () => {
       console.log(`Server listening on port ${this.port}`);
+    });
+
+    this.serverListener.on('error', (err: NodeJS.ErrnoException) => {
+      console.log({ serverError: err });
     });
   }
 
   public close() {
-    console.log('Server closed');
-    this.serverListener?.close();
+    return new Promise((resolve, reject) => {
+      this.serverListener.close((error: Error | undefined) => {
+        if (error) {
+          console.log({ error });
+          reject('There was an error closing the server');
+        } else {
+          console.log('Server closed');
+          resolve(true);
+        }
+      });
+    });
   }
 }
