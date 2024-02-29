4f3d7e9cbb174ffbf32dd48350396c6a32c2b41e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-29 16:48:47 +0100

    feat: modify copy-assets script

--- src/scripts/copy-assets.ts
+++ src/scripts/copy-assets.ts
@@ -2,4 +2,4 @@ import * as shell from 'shelljs';
 
 // Copy all the view templates
 shell.cp('-R', 'src/presentation/web/views', 'dist/presentation/web/views');
-shell.cp('-R', 'src/presentation/locales', 'dist/presentation/locales');
+// shell.cp('-R', 'src/presentation/locales', 'dist/presentation/locales');
